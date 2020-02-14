4fe2c7cdb5f76fae299214a020c5cefed01f0c5f
木蓝茶陌 <1824532697@qq.com>
2020-02-14 17:49:57 +0800

    '图片懒加载和文章列表延迟加载功能完成'

--- src/main.js
+++ src/main.js
@@ -8,11 +8,24 @@ import 'vant/lib/index.css'
 import 'amfe-flexible/index.min.js'
 import '@/utils/validate.js' // 验证相关
 import '@/assets/css/global.css'
+import { Lazyload } from 'vant' // 导入懒加载模块
+Vue.use(Lazyload) // 注册懒加载指令
 Vue.use(Vant)
 Vue.config.productionTip = false
 
+// 创建一个延迟器，
+// 给Vue增加继承成员，名称$sleep，
+Vue.prototype.$sleep = time => {
+    // 返回一个Promise对象，应用端可以设置async/await使得异步顺序执行
+    return new Promise((resolve) => {
+        window.setTimeout(() => {
+            resolve()
+        }, time)
+    })
+}
+
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
--- src/views/home/components/com-article.vue
+++ src/views/home/components/com-article.vue
@@ -1,9 +1,22 @@
 <template>
   <div>
-      <van-pull-refresh v-model="isLoading" @refresh="onRefresh">
-    <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
-      <van-cell v-for="item in articleList" :key="item.art_id.toString()" :title="item.title" />
-    </van-list>
+    <van-pull-refresh v-model="isLoading" @refresh="onRefresh">
+      <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
+        <van-cell v-for="item in articleList" :key="item.art_id.toString()" :title="item.title">
+          <template slot="label">
+            <van-grid v-if="item.cover.type>0" :column-num="item.cover.type" :border="false">
+              <van-grid-item v-for="(item2,key2) in item.cover.images" :key="key2">
+                <van-image width="90" height="90" :src="item2" lazy-load/>
+              </van-grid-item>
+            </van-grid>
+            <p>
+              <span>作者:{{item.aut_name}}</span>
+              <span class="spanspace">评论:{{item.comm_count}}</span>
+              <span>时间:{{item.pubdate}}</span>
+            </p>
+          </template>
+        </van-cell>
+      </van-list>
     </van-pull-refresh>
   </div>
 </template>
@@ -28,7 +41,7 @@ export default {
     }
   },
   methods: {
-    async  getArticleList () {
+    async getArticleList () {
       const result = await apiArticleList({
         channel_id: this.channelId,
         timestamp: this.ts
@@ -41,12 +54,15 @@ export default {
         this.isLoading = false
       }, 1000)
     },
-    async  onLoad () {
+    async onLoad () {
+      // 应用延迟器，使得执行速度减慢
+      await this.$sleep(800) // 该延迟器要执行0.8秒
       // 异步更新数据
       // setTimeout 仅做示例，真实场景中一般为 ajax 请求
       const articles = await this.getArticleList()
       if (articles.results.length > 0) {
         // 更新时间戳
+        console.log(articles.results)
         this.ts = articles.pre_timestamp
         this.articleList.push(...articles.results)
       } else {
@@ -64,5 +80,8 @@ export default {
 .scroll-wrapper {
   height: 100%;
   overflow-y: auto;
+}
+.spanspace{
+margin:0 16px
 }
 </style>
